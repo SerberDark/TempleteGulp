26b656483f2b2695e45ce1b8b9a0ba4761bd8b14
Serhii Omelianenko <serberdark@gmail.com>
2017-01-27 21:17:01 +0200

    создал scss стили

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 
-gulp.task('test', function(){
-	console.log('ferst task done');
-})
+gulp.task('sass', function () {
+  return gulp.src('dist/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/css'));
+});
